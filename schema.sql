f56c83211d8bbee3f5344dccdafd7a2f8ffc68da
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-21 20:28:47 +0600

    Create animals table.

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE vet_clinic;
+
+CREATE TABLE IF NOT EXISTS animals (
+  id INT PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL
+);
