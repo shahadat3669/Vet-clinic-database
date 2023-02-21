129a8ca8edcac0b013edb6604a76369713813c98
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-21 20:34:48 +0600

    Add query to get all animals that are neutered.

--- queries.sql
+++ queries.sql
@@ -7,3 +7,5 @@ SELECT name FROM animals WHERE neutered IS TRUE AND escape_attempts<3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon','Pikachu');
 
 SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
+
+SELECT name FROM animals WHERE neutered IS TRUE;
