ad04a787db93bfce7d3f023a1cc17a360b0348fc
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-22 03:43:27 +0600

    Update all animals' weight to be their weight multiplied by -1.

--- queries.sql
+++ queries.sql
@@ -37,4 +37,6 @@ END;
 BEGIN;
 	DELETE FROM animals WHERE date_of_birth > '01-01-2022';
 	SAVEPOINT DELETED_AFTER_JAN_2022;
+	UPDATE animals SET weight_kg = weight_kg * -1;
+    SELECT * FROM   animals;
 END;
