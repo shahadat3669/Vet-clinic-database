abf8820f84d32332224dbe3a22d5c083fed537b4
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-25 00:06:46 +0600

    Modify inserted animals to include owner information about owner_id.

--- data.sql
+++ data.sql
@@ -38,3 +38,9 @@ UPDATE animals
 UPDATE animals 
   SET species_id = 1 WHERE
      name NOT LIKE '%mon';
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
