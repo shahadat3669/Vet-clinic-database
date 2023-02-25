95bcbb6d510fdbcf6e8d1b727d24df1c96cf353b
Shahadat Hossain <shahadat3669@gmail.com>
2023-02-25 22:50:22 +0600

    Insert four data into vets as requirement.

--- data.sql
+++ data.sql
@@ -44,3 +44,9 @@ UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
 UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
 UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+
+INSERT INTO vets VALUES 
+  (DEFAULT, 'William Tatcher', 45, '04-23-2000'),
+  (DEFAULT, 'Maisy Smith', 26, 'Jan-17-2019'),
+  (DEFAULT, 'Stephanie Mendez', 64, 'May-04-1981'),
+  (DEFAULT, 'Jack Harkness', 38, 'Jun-08-2008');
